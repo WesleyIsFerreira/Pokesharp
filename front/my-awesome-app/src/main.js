0ce42099f530bdff996c3054f5602aa2f4f14ad4
Wesley Ferreira <37143147+WesleyIsFerreira@users.noreply.github.com>
2022-03-08 00:46:11 -0300

    tw-elements adicionado

--- front/my-awesome-app/src/main.js
+++ front/my-awesome-app/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/app.css';
+import 'tw-elements'
 
 createApp(App).use(store).use(router).mount('#app')
